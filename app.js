ac26dcf3a598267ab038184d001d474259b6c465
mehdishokrani <mehdishokrani1@gmail.com>
2023-07-26 16:20:22 -0600

    initial stages for other tabs according to owner needs added

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
 const bcrypt = require('bcrypt');
 
 const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({
+  secret: 'your-secret-key',
+  resave: false,
+  saveUninitialized: false
+}));
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -79,13 +83,14 @@ let loginAttempts = {};
 app.post('/login', async (req, res) => {
   const email = req.body.email;
   const user = users.find(user => user.email === email);
-  
+
   if (user == null) {
-    return res.render('login', { error: 'This Username or Email doesnot exist' });
+    return res.render('login', { error: 'This Username or Email does not exist' });
   }
 
   try {
     if(await bcrypt.compare(req.body.password, user.password)) {
+      req.session.user = user;
       res.redirect('/');
     } else {
       loginAttempts[email] = (loginAttempts[email] || 0) + 1;
@@ -98,7 +103,7 @@ app.post('/login', async (req, res) => {
 
         res.render('login', { error: 'Too many login attempts. Please try again in 10 seconds.', timeout: 10 });
       } else {
-        res.render('login', { error: 'Incorrect password. Please try again. Permitted attmpt: '+ (3-loginAttempts[email])});
+        res.render('login', { error: 'Incorrect password. Please try again. Permitted attempt: '+ (3-loginAttempts[email])});
       }
     }
   } catch {
@@ -106,6 +111,145 @@ app.post('/login', async (req, res) => {
   }
 });
 
+let properties = {};
 
+function checkOwner(req, res, next) {
+  if (!req.session.user || req.session.user.role !== 'Owner') {
+    return res.status(403).send('Unauthorized');
+  }
+  next();
+}
+
+app.get('/properties/new', checkOwner, (req, res) => {
+  res.render('property-new');
+});
+
+app.post('/properties/new', (req, res) => {
+  let id = Date.now().toString();
+  properties[id] = {
+    address: req.body.address,
+    neighborhood: req.body.neighborhood,
+    sqft: req.body.sqft,
+    parking: req.body.parking === 'yes',
+    publicTrans: req.body.publicTrans === 'yes',
+    workspaces: {},
+  };
+  res.redirect('/properties/' + id);
+});
+
+app.get('/properties/:id', (req, res) => {
+  const property = properties[req.params.id];
+  if (!property) {
+    return res.status(404).send('Property not found');
+  }
+  res.render('property', { property });
+});
+
+app.get('/properties/:propertyId/workspaces/new', (req, res) => {
+  const property = properties[req.params.propertyId];
+  if (!property) {
+    return res.status(404).send('Property not found');
+  }
+  res.render('workspace-new', { propertyId: req.params.propertyId });
+});
+
+app.post('/properties/:propertyId/workspaces/new', (req, res) => {
+  const property = properties[req.params.propertyId];
+  if (!property) {
+    return res.status(404).send('Property not found');
+  }
+  const id = Date.now().toString();
+  property.workspaces[id] = {
+    type: req.body.type,
+    seats: req.body.seats,
+    smoking: req.body.smoking === 'yes',
+    availability: req.body.availability,
+    lease: req.body.lease,
+    price: req.body.price,
+  };
+  res.redirect('/properties/' + req.params.propertyId + '/workspaces/' + id);
+});
+
+app.get('/properties/:propertyId/workspaces/:workspaceId', (req, res) => {
+  const property = properties[req.params.propertyId];
+  if (!property || !property.workspaces || !property.workspaces[req.params.workspaceId]) {
+    return res.status(404).send('Workspace not found');
+  }
+  const workspace = property.workspaces[req.params.workspaceId];
+  res.render('workspace', { workspace });
+});
+
+app.get('/properties/:propertyId/edit', (req, res) => {
+  const property = properties[req.params.propertyId];
+  if (!property) {
+    return res.status(404).send('Property not found');
+  }
+  res.render('property-edit', { property });
+});
+
+app.post('/properties/:propertyId/edit', (req, res) => {
+  const propertyId = req.params.propertyId;
+  const property = properties[propertyId];
+
+  if (!property) {
+    return res.status(404).send('Property not found');
+  }
+
+  properties[propertyId] = {
+    ...properties[propertyId],
+    address: req.body.address,
+    neighborhood: req.body.neighborhood,
+    sqft: req.body.sqft,
+    parking: req.body.parking === 'yes',
+    publicTrans: req.body.publicTrans === 'yes',
+  };
+
+  res.redirect('/properties/' + propertyId);
+});
+
+app.get('/properties/:propertyId/workspaces/:workspaceId/edit', (req, res) => {
+  const property = properties[req.params.propertyId];
+  if (!property || !property.workspaces || !property.workspaces[req.params.workspaceId]) {
+    return res.status(404).send('Workspace not found');
+  }
+  const workspace = property.workspaces[req.params.workspaceId];
+  res.render('workspace-edit', { workspace });
+});
+
+app.post('/properties/:propertyId/workspaces/:workspaceId/edit', (req, res) => {
+  const propertyId = req.params.propertyId;
+  const workspaceId = req.params.workspaceId;
+  const property = properties[propertyId];
+
+  if (!property || !property.workspaces || !property.workspaces[workspaceId]) {
+    return res.status(404).send('Workspace not found');
+  }
+
+  property.workspaces[workspaceId] = {
+    type: req.body.type,
+    seats: req.body.seats,
+    smoking: req.body.smoking === 'yes',
+    availability: req.body.availability,
+    lease: req.body.lease,
+    price: req.body.price,
+  };
+
+  res.redirect('/properties/' + propertyId + '/workspaces/' + workspaceId);
+});
+
+app.post('/properties/:propertyId/delete', (req, res) => {
+  delete properties[req.params.propertyId];
+  res.redirect('/properties');
+});
+
+app.post('/properties/:propertyId/workspaces/:workspaceId/delete', (req, res) => {
+  const property = properties[req.params.propertyId];
+  if (!property || !property.workspaces) {
+    return res.status(404).send('Property not found');
+  }
+
+  delete property.workspaces[req.params.workspaceId];
+  res.redirect('/properties/' + req.params.propertyId);
+});
 
 app.listen(3000, () => console.log('Server started on port 3000'));
